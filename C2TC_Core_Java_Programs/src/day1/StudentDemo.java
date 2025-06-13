d2b4e61b55a70931eb51e69d8235918c06d1f14e
aghar <aghar@DESKTOP-ABKI64J.mshome.net>
2025-06-13 11:51:24 +0530

    The program is about class inside another class

---
+++ C2TC_Core_Java_Programs/src/day1/StudentDemo.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.id = 10;
+		st.name = "John";
+		st.age = 20;
+		st.show();
+		//System.out.println(st.id + ":" + st.name + ":"+ st.age);
+
+	}
+
+}
