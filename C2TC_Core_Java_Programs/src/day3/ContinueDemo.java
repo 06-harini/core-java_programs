fe9d37251b5ee370d7a690a1eefcc06e33577214
aghar <aghar@DESKTOP-ABKI64J.lan>
2025-06-15 15:00:24 +0530

    The program contains continue statement and for loop

---
+++ C2TC_Core_Java_Programs/src/day3/ContinueDemo.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		for (int k = 5; k < 25; k++)
+		{
+		// Odd numbers are skipped
+		if (k%2 != 0)
+		continue;
+		// Even numbers are printed
+		System.out.print(k + " ");
+		}
+
+	}
+
+}
