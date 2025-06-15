8ad302b59179b5ef1c571c91092ab23269b94346
aghar <aghar@192.168.31.82>
2025-06-15 21:19:53 +0530

    The program consist of class entities

---
+++ C2TC_Core_Java_Programs/src/day1/StudentsDemo.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class StudentsDemo {
+
+	public static void main(String[] args) {
+		Students st = new Students();
+		st.setId  (101);
+		st.setName  ("John");
+		st.setAge  (20);
+		
+		System.out.println(st);
+		
+		Students st1 = new Students();
+		st.setId  (102);
+		st.setName  ("ragu");
+		st.setAge  (21);
+		
+		System.out.println(st1);
+	
+
+
+		
+	}
+
+}
