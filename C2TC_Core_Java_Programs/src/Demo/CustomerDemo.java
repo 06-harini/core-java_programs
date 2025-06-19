47e6e1a3616a7d7fdeb121903a469b92bf9aa9f5
aghar <aghar@DESKTOP-ABKI64J.mshome.net>
2025-06-19 10:29:26 +0530

    This program is about constructor

---
+++ C2TC_Core_Java_Programs/src/Demo/CustomerDemo.java
@@ -0,0 +1,65 @@
+package Demo;
+
+import java.util.Scanner;
+
+import day4.Customer;
+
+public class CustomerDemo {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String name, city;
+		int id;
+		System.out.print("Enter Customer Id : ");
+		id=sc.nextInt();
+	
+		System.out.print("Enter Customer Name : ");
+		name=sc.nextLine();
+		
+		System.out.print("Enter Customer City : ");
+		city=sc.nextLine();
+
+		
+		System.out.println(" ");
+		
+		Customer c1=new Customer(name, id, city); //default constructor invoked
+		c1.setCustomerName(name);
+		c1.setCustomerId(id);
+		c1.setCustomerCity(city);
+		System.out.println(c1);
+		
+		System.out.print("Enter Customer Id : ");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("Enter Customer Name : ");
+		name=sc.nextLine();
+		
+		System.out.print("Enter Customer City : ");
+		city=sc.nextLine();
+		
+		
+		Customer c2=new Customer(name, id, city); //parameterized constructor invoked);
+		System.out.println(c2);
+		
+		
+		System.out.print("Enter Customer Id : ");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("Enter Customer Name : ");
+		name=sc.nextLine();
+		
+		System.out.print("Enter Customer City : ");
+		city=sc.nextLine();
+		
+		
+		Customer c3=new Customer(name, id, city); //parameterized constructor invoked);
+		System.out.println(c3);
+		
+		sc.close();
+		
+
+	}
+	}
+		
